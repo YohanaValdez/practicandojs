7b3c21a70269a6c57a236e28187ae8840b4d5382
yohana valdez <yohana.valdez@hotmail.com>
2023-02-21 12:09:21 -0300

    practica de js

---
+++ app.js
@@ -0,0 +1,58 @@
+//Pide la edad y si es mayor de 18 años indica que ya puede conducir.
+// console.warn("ejercicio 1");
+
+// let edad = prompt("ingrese la edad");
+
+// if (edad >= 18) {
+//   alert("Puede conducir");
+// }else {
+//   alert("No llega a la edad minima para conducir")
+// }
+
+//Pide una nota (número). Muestra la calificación según la nota:
+// let nota = parseInt(prompt("Ingrese un numero del 0 al 10"))
+// parseInt(devuelve lo ingresado en un numero entero)
+
+// if (nota == 10) {
+//   console.log("Excelente");
+// } else if (nota >= 8 && nota<10 ){
+//   console.log("Muy bien");
+// } else if (nota >= 6 && nota<8 ){
+//   console.log("Bien");
+// } else if (nota >= 4 && nota<6 ){
+//   console.log("Desaprobado");
+// } else  {
+//   console.log("Aplazado");
+// }
+
+//switch
+// switch (nota) {
+//   case 0:0
+//   case 1:1
+//   case 2:2
+//     console.log("Insuficiente");
+//   break;  
+//   case 3:
+//   case 4:
+//   case 5:
+//     console.log("Desaprobado");
+//     break;
+//   case 6:
+//   case 7:
+//     console.log("Aprobado");
+//     break;
+//   case 8:
+//   case 9:
+//     console.log("Muy bien");
+//     break;
+//   default:
+//     console.log("Excelente");
+//     break;
+// }
+
+// Consultar para que no siga evaluando numeros mas grandes al 10
+
+//Realiza un script que pida cadenas de texto  hasta que se pulse 
+//“cancelar”. Al salir con “cancelar” deben mostrarse todas las
+//cadenas concatenadas con un guión -.
+
